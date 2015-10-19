d26c8016b535223673dba82341008cea25657bab
Korbinian Würl <korbinianwuerl@googlemail.com>
2015-10-20 00:04:14 +0200

    user class comments

--- src/Model/User.php
+++ src/Model/User.php
@@ -3,8 +3,7 @@ namespace Model;
 /**
  * the User model class
  *
- * @modified Swathi Shyam Sunder <swathi.ssunder@tum.de>
- * @author Korbinian Würl <korbinianwuerl@googlemail.com>
+ * @author Swathi Shyam Sunder <swathi.ssunder@tum.de>
  */
 class User {
 	private $first_name = "";
@@ -20,30 +19,40 @@ class User {
 	}
 	/**
 	 * Sets the first name
+	 * 
+	 * @param string $firstName
 	 */
 	public function setFirstName($firstName) {
 		$this->first_name = $firstName;
 	}
 	/**
 	 * Sets the last name
+	 * 
+	 * @param string $lastName
 	 */
 	public function setLastName($lastName) {
 		$this->last_name = $lastName;
 	}
 	/**
 	 * Sets the email
+	 * 
+	 * @param string $email
 	 */
 	public function setEmail($email) {
 		$this->email = $email;
 	}
 	/**
 	 * Sets the plain password
+	 * 
+	 * @param string $password
 	 */
 	public function setPasswordPlain($password) {
 		$this->password_plain = $password;
 	}
 	/**
 	 * Sets the active status
+	 * 
+	 * @param boolean $isActive
 	 */
 	public function setIsActive($isActive) {
 		$this->is_active = $isActive;
