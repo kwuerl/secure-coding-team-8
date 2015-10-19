43cfbd85e5845faa0af3d06775b889f058d8c98e
Korbinian Würl <korbinianwuerl@googlemail.com>
2015-10-19 19:21:32 +0200

    form helper initial

---
+++ src/Helper/FormHelper.php
@@ -0,0 +1,10 @@
+<?php
+namespace Helper;
+/**
+ * FormService can be used to sanatize form data and map post data to objects
+ */
+class FormHelper {
+	private $request;
+	private $field_configs;
+	
+}
---
+++ src/Service/FormService.php
@@ -0,0 +1,8 @@
+<?php
+namespace Service;
+/**
+ * FormService can be used to sanatize form data and map post data to objects
+ */
+class FormService {
+	
+}
