cd69fc57bb372ebb1b399fdf0284bc32dd771043
swathi-ssunder <swathi.ssunder@tum.de>
2015-10-22 22:18:27 +0200

    Fix for errors and configuration of db parameters for testing

--- src/Config/parameters.php
+++ src/Config/parameters.php
@@ -4,7 +4,10 @@
  */
 
 $parameter_service->set("example_param", "example_value");
-$parameter_service->set("mysql_host", "example_value");
-$parameter_service->set("mysql_user", "example_value");
-$parameter_service->set("mysql_password", "example_value");
-$parameter_service->set("mysql_database", "example_value");
+$parameter_service->set("mysql_host", "127.0.0.1");
+$parameter_service->set("mysql_user", "root");
+$parameter_service->set("mysql_password", "root");
+$parameter_service->set("mysql_database", "BANK_DETAILS");
+$parameter_service->set("group_user", "USER");
+$parameter_service->set("group_employee", "EMPLOYEE");
+$parameter_service->set("group_admin", "ADMIN");
