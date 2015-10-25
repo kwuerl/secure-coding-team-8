fba07e38419cdb3e59f9f8db5f4b10e7f1da5c08
swathi-ssunder <swathi.ssunder@tum.de>
2015-10-25 17:52:21 +0100

    path change to load files from root

--- src/Template/base.html.php
+++ src/Template/base.html.php
@@ -3,13 +3,13 @@
         <title>SecureBank</title>
         <link rel="icon" type="image/ico" href="favicon.ico"/>
         <!-- Bootstrap 3.3.5 -->
-        <link rel="stylesheet" href="Vendor/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="Vendor/bootstrap/css/dataTables.bootstrap.css">
+        <link rel="stylesheet" href="/Vendor/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/Vendor/bootstrap/css/dataTables.bootstrap.css">
 
         <!-- Theme style -->
-	    <link rel="stylesheet" href="Vendor/theme/AdminLTE.min.css">
-	    <link rel="stylesheet" href="Vendor/theme/header.css">
-	    <link rel="stylesheet" href="Vendor/theme/skin.min.css">
+	    <link rel="stylesheet" href="/Vendor/theme/AdminLTE.min.css">
+	    <link rel="stylesheet" href="/Vendor/theme/header.css">
+	    <link rel="stylesheet" href="/Vendor/theme/skin.min.css">
     </head>
     <body class="hold-transition
     <?php $t->block("body_classes", function ($t) { ?>
@@ -19,11 +19,11 @@
     <?php $t->block("body", function ($t) { ?>
         Default
     <?php }); ?>
-     <script src="Vendor/js/jQuery-2.1.4.min.js"></script>
-     <script src="Vendor/bootstrap/js/bootstrap.min.js"></script>
-     <script src="Vendor/js/jquery.dataTables.min.js"></script>
-     <script src="Vendor/js/dataTables.bootstrap.min.js"></script>
-     <script src="Scripts/app.js"></script>
+     <script src="/Vendor/js/jQuery-2.1.4.min.js"></script>
+     <script src="/Vendor/bootstrap/js/bootstrap.min.js"></script>
+     <script src="/Vendor/js/jquery.dataTables.min.js"></script>
+     <script src="/Vendor/js/dataTables.bootstrap.min.js"></script>
+     <script src="/Scripts/app.js"></script>
     </body>
      <!-- Modals -->
      <!-- Approve Transaction Modal -->
