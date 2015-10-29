5a3fcd5d5999889ac6336d523a9f034277f6c72b
Mai Ton Nu Cam <maitonnucam@googlemail.com>
2015-10-30 00:37:23 +0100

    wip

--- textparser/textparser.c
+++ textparser/textparser.c
@@ -2,56 +2,67 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <math.h>
 
 int main(int argc, char **argv) {
-	int to;
-	char to_name[31];
-	float amount;
-	char code[16];
-	char remarks[129];
+	char account_id[10] = {0};
+	char account_name[30] = {0};
+	char amount[8] = {0};
+	char code[15] = {0};
+	char remarks[128] = {0};
 
 	char *input = argv[1];
 	FILE *input_file;
-	char read[129];
 
-	input_file = fopen("test.txt", "r");
+	input_file = fopen(input, "r");
 
 	if (input_file == NULL) {
-		perror("Canot open input file\n");
+		perror("Cannot open input file\n");
         return(-1);
 	} else {
-		for (int i = 0; i < 5; i++) {
-			if (i == 0) {
-				if (fgets(read, 12, input_file) != NULL) {
-					sscanf(read, "%d", &to);
+		int i = 0;
+		int j = 0;
+		char c;
+		do {
+			if ((c = fgetc(input_file)) != '\n') {
+				if (i == 0) {
+					if (j < sizeof(account_id)) {
+						account_id[j] = c;
+						j++;
+					}
+				} else if (i == 1) {
+					if (j < sizeof(account_name)) {
+						account_name[j] = c;
+						j++;
+					}
+				} else if (i == 2) {
+					if (j < sizeof(amount)) {
+						amount[j] = c;
+						j++;
+					}
+				} else if (i == 3) {
+					if (j < sizeof(code)) {
+						code[j] = c;
+						j++;
+					}
+				} else if (i == 4) {
+					if (j < sizeof(remarks)) {
+						remarks[j] = c;
+						j++;
+					}
 				}
-			} else if (i == 1) {
-				if (fgets(read, 32, input_file) != NULL) {
-					read[strcspn(read, "\n")] = 0;
-					strcpy(to_name, read);
-				}
-			} else if (i == 2) {
-				if (fgets(read, 10, input_file) != NULL) {
-					sscanf(read, "%F", &amount);
-				}
-			} else if (i == 3) {
-				if (fgets(read, 17, input_file) != NULL) {
-					sscanf(read, "%s", &code);
-				}
-			} else if (i == 4) {
-				if (fgets(read, 130, input_file) != NULL) {
-					read[strcspn(read, "\n")] = 0;
-					strcpy(remarks, read);
-				}
+			} else {
+				i++;
+				j = 0;
 			}
-		}
+
+	    } while (c != EOF);
 	}
-	printf("%d\n", to);
-	printf("%s\n", to_name);
-	printf("%F\n", amount);
-	printf("%s\n", code);
-	printf("%s\n", remarks);
 	fclose(input_file);
+
+	char json[250];
+	snprintf(json, sizeof(json), "{\"account_id\":%s,\"account_name\":\"%s\",\"amount\":%s,\"code\":\"%s\",\"remarks\":\"%s\"}", account_id, account_name, amount, code, remarks);
+	printf("%s", json);
 
 	return 0;
 }
